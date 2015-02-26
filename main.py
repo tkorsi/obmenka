b993dec23751285ced3ec6025d3d378b73053e18
Iegor Shapanov <hiiegor@gmail.com>
2015-02-26 15:59:52 +0200

    Golf GTI VII price

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+#-*- coding: utf-8-*-
 import time
 
 import requests
 
 from lxml.html import fromstring
 from pync import Notifier
+import re
 
-ENDPOINT = 'http://obmenka.kharkov.ua/'
+ENDPOINT = 'http://atlant-m.kiev.ua/ru/models/new_golf/facts-figures/prices/11-highline/'
 TIMEOUT = 5
-XPATH = '/html/body/div[2]/div[2]/div[1]/table/tbody/tr[1]/td[5]'
-TITLE = 'UAH per USD'
+XPATH = '/html/body/div[1]/div[2]/div[3]/div/div[2]/table/tbody/tr[6]/td[7]'
+TITLE = 'Golf GTI VII'
 
 
 def main():
@@ -20,8 +22,9 @@ def main():
             html = requests.get(ENDPOINT).text
             tree = fromstring(html)
             el = tree.xpath(XPATH)[0]
-            value = float(el.text.replace(',', '.'))
-        except Exception:
+            value =int(''.join(re.findall(r'\d', el.text)))
+        except Exception as e:
+	    print(e)
             time.sleep(TIMEOUT)
             continue
 
@@ -32,9 +35,10 @@ def main():
                 Notifier.notify(
                     TITLE,
                     open=ENDPOINT,
-                    title=str(value)
+                    title=unicode(value) + u' uah'
                 )
-            except Exception:
+            except Exception as e:
+		print(e)
                 pass
 
         time.sleep(TIMEOUT)
